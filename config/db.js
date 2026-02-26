5318439fa339ba871de4bd673355aa60a9397618
PaoloFumachi <cesarfumachi2002@gmail.com>
2026-02-25 20:51:58 -0500

    Preparación para Railway

--- config/db.js
+++ config/db.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 // FORZAR ZONA HORIA AMÉRICA/LIMA EN TODO EL POOL
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST || process.env.DB_HOST, // Railway usa MYSQLHOST
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
